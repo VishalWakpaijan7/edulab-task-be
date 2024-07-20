9c8383f53f83407770d347f737d902bd2f84d632
Vishal W <wakpaijanvishal@gmail.com>
2024-07-20 13:35:07 +0530

    cors middleware added

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const pdfGenRouter = require('./routes/pdf-generator');
 const { errorHandler, notFoundHandler } = require('./middlewares/errorhandler');
 const PORT = 8080;
 
+const corsOptions = {
+    origin: 'http://localhost:4200', // Allow only this origin
+    methods: ['GET'], // Allow only these methods
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api', pdfGenRouter);
 app.use(notFoundHandler);
